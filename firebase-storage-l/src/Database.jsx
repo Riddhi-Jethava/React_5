224e57fb91d5e82c7c0ddf283c0ae49a275b0d63
Riddhi Jethava <riddhijethava08@gmail.com>
2024-07-09 22:40:41 +0530

    app

--- firebase-storage-l/src/Database.jsx
+++ firebase-storage-l/src/Database.jsx
@@ -1,18 +1,37 @@
 import React, { useState } from 'react'
 import { ref, set } from "firebase/database";
+import { database } from './Firebase';
 
 function Database() {
-    const [databas, setDatabase] = useState('')
-  return (
-    <div>
-    <form action="">
-        <label htmlFor="">Name: </label>
-        <input type="text" value={name}/>
-        <label htmlFor="">Password: </label>
-        <input type="password" value={password}/>
+  const [name, setname] = useState('')
+  const [password, setpassword] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    const newRef = ref(database, `user${name}`)
+    set(newRef, {name, password})
+    .then(()=>{
+      alert('Data add successfully')
+    })
+    .catch(()=>{
+      alert('Data add unsuccessfully')
+    })
+
+    setname('')
+    setpassword('')
+  }
+
+return (
+  <div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="">Name: </label>
+      <input type="text" value={name} onChange={(e) => setname(e.target.value)} /> <br /><br />
+      <label htmlFor="">Password: </label>
+      <input type="password" value={password} onChange={(e) => setpassword(e.target.value)} /> <br /><br />
+      <button type='submit'>submit</button>
     </form>
-    </div>
-  )
+  </div>
+)
 }
 
 export default Database
--- firebase-storage-l/src/Firebase.jsx
+++ firebase-storage-l/src/Firebase.jsx
@@ -1,10 +1,11 @@
-
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBCvJH1tILtSsimeZ6cTDgKqPZARzK55rk",
   authDomain: "myproject-a4873.firebaseapp.com",
+  databaseURL: "https://myproject-a4873-default-rtdb.firebaseio.com",
   projectId: "myproject-a4873",
   storageBucket: "myproject-a4873.appspot.com",
   messagingSenderId: "94885953128",
@@ -12,5 +13,8 @@ const firebaseConfig = {
   measurementId: "G-PC7JVCEQ64"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+const database = getDatabase(app);  
+
+export {database}
